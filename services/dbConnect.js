be06bd9e9a16700967ba23f90d89a0856cb98009
Jun Hau <phuajunhau2001.jp@gmail.com>
2024-03-19 21:33:43 +0800

    -solve application error problem after deployed

--- services/dbConnect.js
+++ services/dbConnect.js
@@ -6,6 +6,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 // configuration details
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+
 // if project has been deployed, connect with the host's DATABASE_URL
 // else connect with the local DATABASE_URL
 const pool = new pg.Pool({
@@ -18,35 +19,35 @@ pool.on('connect', () => {
   console.log('Teamwork Database connected successfully!');
 });
 
-const createTables = () => {
-  const imageTable = `CREATE TABLE IF NOT EXISTS
-    images(
-      id SERIAL PRIMARY KEY,
-      title VARCHAR(128) NOT NULL,
-      cloudinary_id VARCHAR(128) NOT NULL,
-      image_url VARCHAR(128) NOT NULL
-    )`;
-  pool
-    .query(imageTable)
-    .then((res) => {
-      console.log(res);
-      pool.end();
-    })
-    .catch((err) => {
-      console.log(err);
-      pool.end();
-    });
-};
+// const createTables = () => {
+//   const imageTable = `CREATE TABLE IF NOT EXISTS
+//     images(
+//       id SERIAL PRIMARY KEY,
+//       title VARCHAR(128) NOT NULL,
+//       cloudinary_id VARCHAR(128) NOT NULL,
+//       image_url VARCHAR(128) NOT NULL
+//     )`;
+//   pool
+//     .query(imageTable)
+//     .then((res) => {
+//       console.log(res);
+//       pool.end();
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       pool.end();
+//     });
+// };
 
-pool.on("remove", () => {
-  console.log("client removed");
-  process.exit(0);
-});
+// pool.on("remove", () => {
+//   console.log("client removed");
+//   process.exit(0);
+// });
 
-//export pool and createTables to be accessible  from an where within the application
-module.exports = {
-  createTables,
-  pool,
-};
+// //export pool and createTables to be accessible  from an where within the application
+// module.exports = {
+//   createTables,
+//   pool,
+// };
 
-require("make-runnable");
+// require("make-runnable");
